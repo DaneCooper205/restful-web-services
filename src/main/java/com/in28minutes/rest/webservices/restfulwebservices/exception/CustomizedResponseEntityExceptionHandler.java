d42acf3cc6e741693c31e9f6071fa26f54606f39
Dane Cooper <dane.cooper@use.startmail.com>
2024-02-08 20:13:52 -0600

    renamed the class to fix capitalization error

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.in28minutes.rest.webservices.restfulwebservices.exception;
+
+import java.time.LocalDate;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+		ErrorDetails errorDetails = new ErrorDetails( LocalDate.now(),ex.getMessage(), request.getDescription(false) );
+		
+		return new ResponseEntity<Object>( errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	
+
+}
