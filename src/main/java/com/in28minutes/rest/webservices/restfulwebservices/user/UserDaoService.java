64e3598c121a2257c124280c17a3c7955e338bcb
Dane Cooper <dane.cooper@use.startmail.com>
2024-02-09 20:38:10 -0600

    added i18n, api generation, validation, and content return

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -35,5 +35,10 @@ public class UserDaoService {
 		users.add(user);
 		return user;
 	}
+	
+	public boolean deleteById(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		return users.removeIf(predicate);
+	}
 
 }
